53bbc271a692c50eb1586a778a2ba6469481a214
Precious Mzembe <100703488+preciousMzembe@users.noreply.github.com>
2025-08-08 09:16:49 +0200

    Delete lib directory

--- lib/controllers/navigation/nav_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class NavController extends GetxController {
-  RxInt pageIndex = 0.obs;
-
-  void changePageIndex(int index) {
-    pageIndex.value = index;
-  }
-}
--- lib/models/lesson/lesson_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:edgiprep/models/lesson/slide_model.dart';
-
-class LessonModel {
-  final String id;
-  final String title;
-  final List<SlideModel> slides;
-
-  LessonModel({required this.id, required this.title, required this.slides});
-}
